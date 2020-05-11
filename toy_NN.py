52cfbe12b853be4d4b240a0bb7edd5fceefd0a69
rymuelle <rmueller@physics.tamu.edu>
2020-05-11 15:21:50 -0500

    added softmax loss function

--- toy_NN.py
+++ toy_NN.py
@@ -2,9 +2,10 @@ import numpy as np
 
 nCategories=4
 inDim=5
-datasets=np.random.rand(nCategories,inDim)-.5
-print
-ys = [i for i in range(nCategories)]
+datasets=np.random.rand(nCategories,inDim)
+i=np.linspace(0,3,4,dtype='int')
+y1 =np.zeros((4,4))
+y1[i,i] = 1
 
 nNodes1=7
 nNodes2=nCategories
@@ -12,7 +13,6 @@ class toy_NN:
     def __init__(self,inDim,nNodes1,nNodes2,learningRate):
         self.w1=np.random.rand(inDim,nNodes1)-.5
         self.b1=np.random.rand(nNodes1)-.5
-        
         self.w2=np.random.rand(nNodes1,nNodes2)-.5
         self.b2=np.random.rand(nNodes2)-.5
         self.learningRate = learningRate
@@ -23,12 +23,20 @@ class toy_NN:
 
     def softMaxDer(self, values, y):
         exponent = np.exp(values)
-        derivative = -(exponent[y]+exponent)/np.sum(np.power(exponent,2))
-        derivative[y]=(exponent[y]+(np.sum(exponent))-exponent[y])/np.sum(np.power(exponent,2))
+        correctExp=np.sum(np.multiply(exponent,y),1,keepdims=True)
+        invertedY=(1-y)
+        offDiagonal = invertedY*correctExp
+        onDiagonal=np.sum(invertedY*exponent,1,keepdims=True)*y
+        sumMatrix=offDiagonal+onDiagonal
+        summedExponent=np.sum(exponent,1,keepdims=True)
+        derivative = (exponent+sumMatrix)/np.power(summedExponent,2)*(-invertedY+y)
         return derivative
 
     def loss(self,prob,y):
-        return np.log(prob[y])*-1, -1/prob[y]
+        correctProb = np.sum(np.multiply(prob,y),1,keepdims=True)
+        return -np.log(correctProb)
+    def lossDerivative(self,loss):
+        return -np.power(loss,-1)
 
     def forwardPass(self,dataset):
         self.firstLayer = datasets.dot(self.w1)
@@ -40,7 +48,13 @@ class toy_NN:
         return softMaxOutput
 
     def train(self, datasets, y):
-        self.forwardPass(datasets)
+        softMaxOutput = self.forwardPass(datasets)
+        loss=self.loss(softMaxOutput,y)
+        print softMaxOutput
+        lossDerivative=self.lossDerivative(loss)
+        print self.softMaxDer(softMaxOutput,y)
+
+
         #for count, dataset in enumerate(datasets):
         #    softMaxOutput=self.forwardPass(dataset)
         #    loss,der=self.loss(softMaxOutput,y[count])
@@ -54,4 +68,4 @@ class toy_NN:
 
 rand_net = toy_NN(inDim,nNodes1,nNodes2,1e-4)
 
-rand_net.train(datasets,ys)
+rand_net.train(datasets,y1)
